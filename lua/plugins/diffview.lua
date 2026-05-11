7dd408e8881d1e9f3b7149943d40e55e08df857a
Kevin Le <kebinhleh@gmail.com>
2026-05-11 10:48:36 -0500

    diffview branch vs dev

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,23 +1,38 @@
 return {
-  'sindrets/diffview.nvim',
-  cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewFileHistory' },
-  keys = {
-    {
-      '<leader>hd',
-      function()
-        if next(require('diffview.lib').views) == nil then
-          vim.cmd('DiffviewOpen')
-        else
-          vim.cmd('DiffviewClose')
-        end
-      end,
-      desc = 'Toggle Diffview',
-    },
-    {
-      '<leader>gh',
-      '<cmd>DiffviewFileHistory %<cr>',
-      desc = 'Diffview File History',
-    },
-  },
-  opts = {},
-}
+   'sindrets/diffview.nvim',
+   cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewFileHistory' },
+   keys = {
+     {
+       '<leader>hd',
+       function()
+         if next(require('diffview.lib').views) == nil then
+           vim.cmd('DiffviewOpen')
+         else
+           vim.cmd('DiffviewClose')
+         end
+       end,
+       desc = 'Toggle Diffview',
+     },
+     {
+       '<leader>gh',
+       '<cmd>DiffviewFileHistory %<cr>',
+       desc = 'Diffview File History',
+     },
+     {
+       '<leader>gb',
+       function()
+         local base = vim.fn.system('git rev-parse --verify --quiet origin/dev'):gsub('%s+', '')
+         if base == '' then
+           base = vim.fn.system('git rev-parse --verify --quiet dev'):gsub('%s+', '')
+         end
+         if base == '' then
+           vim.notify('No dev branch found (tried origin/dev, dev)', vim.log.levels.ERROR)
+           return
+         end
+         vim.cmd('DiffviewOpen ' .. base .. '...HEAD')
+       end,
+       desc = 'Diffview branch vs dev',
+     },
+   },
+   opts = {},
+ }
